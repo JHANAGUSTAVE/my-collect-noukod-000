c311e688cd63b05bc218577d13f29dd863e810cb
Jhana Gustave <jhana.gustave@gmail.com>
2019-04-24 15:46:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -7,9 +7,10 @@ def my_collect(collection)
       i = i + 1
     end
    return collection
-      collection = ['ruby', 'javascript', 'python', 'objective-c']
-          my_collect(collection) do |lang|
-           lang.upcase
+          array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+          my_collect(array) do |name|
+           name.split(" ").first
+    end
 end
 end
  
